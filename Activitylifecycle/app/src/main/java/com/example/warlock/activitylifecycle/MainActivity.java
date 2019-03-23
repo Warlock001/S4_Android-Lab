ebba3fc57252017edaa3367c63a109a3e946fa04
Warlock001 <alfinwilliam@gmail.com>
2019-03-23 18:44:51 +0530

    final commit

--- Activitylifecycle/app/src/main/java/com/example/warlock/activitylifecycle/MainActivity.java
+++ Activitylifecycle/app/src/main/java/com/example/warlock/activitylifecycle/MainActivity.java
@@ -11,36 +11,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // making toast to display text
-        Toast.makeText(this,"OnCreate",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"OnCreate Invoked",Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onStart() {
         super.onStart();
-        Toast.makeText(this,"Onstart",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Onstart Invoked",Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onRestart() {
         super.onRestart();
-        Toast.makeText(this,"Onrestart",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Onrestart Invoked",Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this,"Onresume",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Onresume Invoked",Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this,"OnPause",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"OnPause Invoked",Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this,"onStop",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"onStop Invoked",Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this,"Ondestroy",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Ondestroy Invoked",Toast.LENGTH_SHORT).show();
     }
 }
