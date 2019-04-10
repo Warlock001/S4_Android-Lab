71801fc090ed0cd5af38f57627cafb59e73d6cf3
Warlock001 <alfinwilliam@gmail.com>
2019-04-10 08:50:46 +0530

    dialog box commit

--- cycle 2/dialogbox/app/src/main/java/com/example/warlock/dialogbox/MainActivity.java
+++ cycle 2/dialogbox/app/src/main/java/com/example/warlock/dialogbox/MainActivity.java
@@ -1,23 +1,33 @@
 package com.example.warlock.dialogbox;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import java.util.Calendar;
 
+import static java.lang.String.valueOf;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText bdob;
+    EditText bdob,corse;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bdob=findViewById(R.id.dob);
-
+        corse=findViewById(R.id.course);
 
 
         bdob.setOnClickListener(new View.OnClickListener() {
@@ -39,5 +49,92 @@ public class MainActivity extends AppCompatActivity {
                 mDatePicker.show();
             }
         });
+
+
+        corse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog;
+
+                final String[] items = {"DBMS", "FP", "AJP"};
+
+                final String[] topics=new String[3]; //String Array Declared
+                topics[0]="DBMS"; // Value intialized
+                topics[1]="FP";
+                topics[2]="AJP";
+
+
+
+                final ArrayList itemsSelected = new ArrayList();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("Select Elective");
+
+                builder.setMultiChoiceItems(items, null,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedItemId,
+                                                boolean isSelected) {
+                                if (isSelected) {
+
+                                    itemsSelected.add(selectedItemId);
+                                } else if (itemsSelected.contains(selectedItemId)) {
+
+                                    itemsSelected.remove(Integer.valueOf(selectedItemId));
+                                }
+                            }
+                        })
+                        .setPositiveButton("Done!", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                String a=valueOf(itemsSelected.get(0));
+                                int b=Integer.parseInt(a);
+                                corse.append(topics[b]);
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+
+                            }
+                        });
+
+                dialog = builder.create();
+
+                dialog.show();
+
+            }
+        });
+
+      /*  btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Confirmation");
+                builder.setMessage("Confirm all form data?");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Form data submitted.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Form data cancelled", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.show();
+            }
+        }); */
+
     }
 }
